80b89a93378cef011881ce2bdd3f668026d11498
diegoBravo-dev <a2270193@correo.uia.mx>
2025-11-18 23:57:38 -0600

    Se añadió una función que verifica si el PIN con el PWM está funcionando de forma correcta

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import modules.motors as motors
 
 motorRight = motors.Motor()
-motorLeft = motors.Motor(16)
+motorLeft = motors.Motor(16, 1000, 17, 18)
+
+"""PRUEBAS PARA VERIFICAR LOS MÉTODOS"""
+
+motorRight.check_motor()
--- modules/motors.py
+++ modules/motors.py
@@ -1,9 +1,10 @@
 from machine import PWM, Pin
+from time import sleep
 
 PWM_FREQ = 1000  # Frecuencia del PWM en HZ
 pin_pwm = 15 # Número de pin GPIO para PWM, se puede cambiar según sea necesario
-pin_enable1 = 14
-pin_enable2 = 13
+pin_enable1 = 14 # Número de pin GPIO para el enable 1/3, se puede cambiar según sea necesario
+pin_enable2 = 13 # Número de pin GPIO para el enable 2/4, se puede cambiar según sea necesario
 
 
 class Motor:
@@ -15,9 +16,16 @@ class Motor:
             self.enable1 = Pin(enable1, Pin.OUT) # Habilita el pin 1 de control del motor
             self.enable2 = Pin(enable2, Pin.OUT) # Habilita el pin 2 de control del motor
             self.led_estatus = Pin("LED", Pin.OUT)
+            self.enable1.value(0) # Enciende el enable 1
+            self.enable2.value(0) # Enciende el enable 2
             self.led_estatus.value(1)  # Enciende el LED de estatus para indicar que el motor está inicializado
+            print("Motor inicializado en el pin PWM:")
             print(pin)
+            print("Frecuencia PWM:")
             print(freq)
+            print("Confirmacion de pines de habilitacion:")
+            print("Enable 1:", enable1)
+            print("Enable 2:", enable2)
         except Exception as e:
             print("Error al inicializar el motor:", e)
 
@@ -52,3 +60,16 @@ class Motor:
     def cleanup(self):
         """Liberar los recursos de PWM."""
         self.pwm.deinit()
+    
+    def check_motor(self):
+        """Revisar si el PWM asignado está bien"""
+
+        while True:
+            for duty in range(0, 65535, 100): # Incrementa el ciclo de trabajo 
+                self.pwm.duty_u16(duty)
+                print(f"Duty cycle: {duty}\n")
+                sleep(0.01)
+            for duty in range(65535, 0, -100): # Decrementa el ciclo de trabajo
+                self.pwm.duty_u16(duty)
+                print(f"Duty cycle: {duty}\n")
+                sleep(0.01)
